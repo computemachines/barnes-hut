1ad27d60142fd9257aed183cb7d711ece9abfff0
Tyler Parker <computemachines@gmail.com>
2016-11-18 12:01:07 -0500

    Explicit testing.

--- test/Spec.hs
+++ test/Spec.hs
@@ -1,10 +1,24 @@
-{-# LANGUAGE TemplateHaskell #-}
-
 import NBody
 
-import Test.QuickCheck
-import Test.QuickCheck.All
+import Test.Framework (defaultMain, testGroup)
+import Test.Framework.Providers.QuickCheck2 (testProperty)
+import Test.QuickCheck2
+
 import Control.Applicative
+
+main :: IO ()
+main = defaultMain tests
+
+tests = [
+  testGroup "Geometric" [
+      testProperty "prop add identity" prop_VectorAddId
+      , testProperty "prop " prop_VectorAddId
+      , testProperty "magnitude is nonnegative" prop_VectorMagPosNN
+      , testProperty "Triangle Inequality Theorem" prop_VectorTriangleIneq
+      , testProperty "Necessary 
+                        ]
+  ]
+
 
 instance Arbitrary Vector where
   arbitrary = Vector <$> arbitrary <*> arbitrary
@@ -12,16 +26,18 @@ instance Arbitrary Vector where
 instance Arbitrary Region where
   arbitrary = Square <$> arbitrary <*> arbitrary
 
--- deepCheck = quickCheckWith stdArgs { maxSuccess = 1000 }
 
-prop_VectorAddId v = v + 0 == v
-prop_VectorMultId v = v * Vector 1 1 == v
+-- Algebra tests
+prop_VectorAddId a = zero +' a == a
+prop_VectorAddAssoc a b = a +' b == b +' a
+prop_VectorAddInv a = a -' a == zero
+prop_VectorMultId v = v `dot` one == v
+prop_VectorMultAssoc a b = a `dot` b == b `dot` a
+prop_VectorDistribMultAdd a b c = a `dot` (b +' c) == (a `dot` b) +' (a `dot c)
+
 prop_VectorMagPos v = classify (magnitude v == 0) "trivial" $ magnitude v >= 0
 prop_VectorTriangleIneq a b = magnitude (a+b) <= magnitude a + magnitude b
 prop_RegionContainsCenter region@(Square center r) = region `contains` center
 prop_RegionPartitionCover region testVector
   = contains region testVector ==> any (`contains` testVector) $ partition region
 
-return []
-main :: IO (Bool)
-main = $quickCheckAll
